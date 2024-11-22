e4e17b09298eb490d514757b5394ccef1b22025a
Mirshod Allaberganov <mir.allaberganov@gmail.com>
2024-11-22 10:38:08 +0500

    Hide whole field instead of set null value

--- src/main/java/uz/mirix/aop/fields/RoleBasedFieldHidingAspect.java
+++ src/main/java/uz/mirix/aop/fields/RoleBasedFieldHidingAspect.java
@@ -1,10 +1,13 @@
 package uz.mirix.aop.fields;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
@@ -21,6 +24,19 @@ import java.util.stream.Collectors;
 @Aspect
 @Component
 public class RoleBasedFieldHidingAspect {
+    private final ObjectMapper objectMapper;
+
+    public RoleBasedFieldHidingAspect(ObjectMapper objectMapper) {
+        this.objectMapper = configureObjectMapper(objectMapper);
+    }
+
+    private ObjectMapper configureObjectMapper(ObjectMapper objectMapper) {
+        return objectMapper
+                .registerModule(new JavaTimeModule())
+                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                .configure(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT, true);
+    }
 
     @Around(
             "@within(uz.mirix.annotations.RoleVisibilityController) && " +
@@ -55,16 +71,23 @@ public class RoleBasedFieldHidingAspect {
             return null;
         }
 
-        if (!(object instanceof Collection) && !object.getClass().isAnnotationPresent(RoleVisibility.class)) {
+        if (object instanceof String || object.getClass().isPrimitive() || !object.getClass().isAnnotationPresent(RoleVisibility.class)) {
             return object;
         }
 
         if (object instanceof Collection) {
-            for (Object item : (Collection<?>) object) {
-                processObject(item, userRoles);
-            }
-            return object;
+            return ((Collection<?>) object).stream()
+                    .map(item -> {
+                        try {
+                            return processObject(item, userRoles);
+                        } catch (IllegalAccessException e) {
+                            throw new RuntimeException("Error processing collection item", e);
+                        }
+                    })
+                    .collect(Collectors.toList());
         }
+
+        ObjectNode objectNode = objectMapper.valueToTree(object);
 
         for (Field field : object.getClass().getDeclaredFields()) {
             if (field.isAnnotationPresent(VisibleForRoles.class)) {
@@ -79,13 +102,21 @@ public class RoleBasedFieldHidingAspect {
                 }
 
                 if (!isVisible) {
-                    field.setAccessible(true);
-                    field.set(object, null);
+                    objectNode.remove(field.getName());
+                } else if (field.getType().isAnnotationPresent(RoleVisibility.class)) {
+                    try {
+                        field.setAccessible(true);
+                        Object nestedObject = field.get(object);
+                        Object processedNestedObject = processObject(nestedObject, userRoles);
+                        objectNode.set(field.getName(), objectMapper.valueToTree(processedNestedObject));
+                    } catch (Exception e) {
+                        throw new RuntimeException("Error processing nested object", e);
+                    }
                 }
             }
         }
 
-        return object;
+        return objectMapper.convertValue(objectNode, object.getClass());
     }
 }
 
